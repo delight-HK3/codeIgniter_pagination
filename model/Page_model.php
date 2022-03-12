a99b52b1ff09b914552cb7e1928cddf03a0398a7
leebean <74585673+delight-HK3@users.noreply.github.com>
2022-03-12 11:32:53 +0900

    Update Page_model.php

--- model/Page_model.php
+++ model/Page_model.php
@@ -2,11 +2,11 @@
 class Page_model extends CI_Model {
 	public function getlist($start,$limit)//정보목록보기
 	{
-		$sql="select * from <테이블 이름> order by no DESC limit $start,$limit";    
+		$sql="select * from pagedb order by no DESC limit $start,$limit";    
 		return $this->db->query($sql)->result();// 쿼리실행, 결과 리턴
 	}
 	public function rowcount(){
-		$sql="select * from <테이블 이름>";
+		$sql="select * from pagedb";
 		return $this->db->query($sql)->num_rows();
 	}
 }
